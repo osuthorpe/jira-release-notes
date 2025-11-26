e9df5593e8d036fcc7f2b2f2cc4c9d4d8d844f25
Alex Thorpe <osuthorpe@gmail.com>
2025-11-26 13:34:23 -0500

    refactor: rename `path` parameter to `collection_path` and update its usage in `pytest_ignore_collect`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 import os
 
-def pytest_ignore_collect(path, config):
+def pytest_ignore_collect(collection_path, config):
     """
     Return True to prevent considering this path for collection.
     This hook is consulted for all files and directories prior to calling
     more specific collection hooks.
     """
     # Check if we are looking at the evaluation or unit test file
-    if path.basename in ["test_evaluation.py", "test_unit.py"]:
+    if collection_path.name in ["test_evaluation.py", "test_unit.py"]:
         # If OPENAI_API_KEY is not set, ignore this file
         if not os.getenv("OPENAI_API_KEY"):
             return True
